7064c8e522e228d364b08a0081cbee37462acbfb
GeonHui2 <92250144+GeonHui2@users.noreply.github.com>
2022-06-27 23:49:51 +0900

    Create Access modifier.java

---
+++ fast/chapter02/Access modifier.java
@@ -0,0 +1,70 @@
+package ch10;
+
+public class BirthDay {
+
+	private int day;
+	private int month;
+	private int year;
+	
+	private boolean isValid;
+	
+	public int getDay() {
+		return day;
+	}
+	
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		if(month < 1 || month > 12) {
+			isValid = false;
+		}
+		else {
+		isValid = true;
+		this.month = month;	
+		}
+		
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public void showDate() {
+		
+		if(isValid) {
+			System.out.println(year + "년" + month + "월" + day + "일 입니다.");
+		}
+		else {
+			System.out.println("유효하지 않은 날짜입니다.");
+		}
+	}
+	
+}
+
+
+package ch10;
+
+public class BirthDayTest {
+
+	public static void main(String[] args) {
+
+		BirthDay date = new BirthDay();
+		
+		date.setYear(2019);
+		date.setMonth(12);
+		date.setDay(30);
+		
+		date.showDate();
+	}
+
+}
