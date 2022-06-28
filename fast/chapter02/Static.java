344d52217de26e189c9edba22d7cef038808b89d
GeonHui2 <92250144+GeonHui2@users.noreply.github.com>
2022-06-28 22:16:25 +0900

    Create Static.java

---
+++ fast/chapter02/Static.java
@@ -0,0 +1,56 @@
+package ch16;
+
+public class Employee {
+
+	public static int serialNum = 1000;
+	
+	private int employeeId;
+	private String employeeName;
+	private String department;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	
+}
+
+package ch16;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+
+		Employee employeeLee = new Employee();
+		employeeLee.setDepartment("이순신");
+		System.out.println(employeeLee.serialNum);
+		
+		Employee employeeKim = new Employee();
+		employeeKim.setEmployeeName("김유신");
+		employeeKim.serialNum++;
+		
+		System.out.println(employeeLee.serialNum);
+		System.out.println(employeeKim.serialNum);
+	}
+
+}
