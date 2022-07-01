fd6dfeec361dc5be0b9f7bacf381ce90197292f4
GeonHui2 <92250144+GeonHui2@users.noreply.github.com>
2022-07-01 20:33:57 +0900

    Create ArrayListTest.java

---
+++ fast/chapter02/ArrayListTest.java
@@ -0,0 +1,84 @@
+package ch24;
+
+import java.util.ArrayList;
+
+public class Student {
+
+	int studentID;
+	String studentName;
+	ArrayList<Subject> subjectList;
+	
+	public Student(int studentID, String studentName) {
+		this.studentID = studentID;
+		this.studentName = studentName;
+		
+		subjectList = new ArrayList<Subject>();
+	}
+	
+	public void addSubject(String name, int score) {
+		Subject subject = new Subject();
+		
+		subject.setName(name);
+		subject.setScorePoint(score);
+		subjectList.add(subject);
+	}
+	
+	public void showStudentInfo() {
+		int total = 0;
+		
+		for(Subject s : subjectList) {
+			
+			total += s.getScorePoint();
+			System.out.println("학생" + studentName + "의" + s.getName() + "과목 성적은 " + s.getScorePoint() + "입니다.");
+		}
+		
+		System.out.println("학생 " + studentName + "의 총점은 " + total + "입니다.");
+	}
+}
+
+package ch24;
+
+public class Subject {
+
+	private String name;
+	private int scorePoint;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScorePoint() {
+		return scorePoint;
+	}
+	public void setScorePoint(int scorePoint) {
+		this.scorePoint = scorePoint;
+	}
+	
+	
+}
+
+package ch24;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+
+		Student studentLee = new Student(1001, "Lee");
+		
+		studentLee.addSubject("국어", 100);
+		studentLee.addSubject("수학", 50);
+		
+		Student studentKim = new Student(1002, "Kim");
+		
+		studentKim.addSubject("국어", 70);
+		studentKim.addSubject("수학", 85);
+		studentKim.addSubject("영어", 100);
+		
+		studentLee.showStudentInfo();
+		System.out.println("======================================");
+		studentKim.showStudentInfo();
+	}
+
+}
